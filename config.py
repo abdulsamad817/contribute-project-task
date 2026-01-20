d9ca90c682136c1f862399d3071a09a61b653821
abdulsamad817 <kinfhgdgslzkxnxnx@gmail.com>
2026-01-20 14:39:43 -0800

    Setup environment variables with .env

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
-MONGODB_URI = "mongodb+srv://crownjude12_db_user:0ysaqe9tjUGNt38V@cluster0.qlqxszg.mongodb.net/auth_db?appName=Cluster0"
-SECRET_KEY = "your_secret_key_change_in_production"
-DEBUG = True
+import os
+from dotenv import load_dotenv
 
-EMAIL_ADDRESS = "crownjude33@gmail.com"
+load_dotenv()
 
-ZEROBOUNCE_API_KEY = "8d5feda44579414a89126ac62be0c60c" 
-  
-SENDGRID_API_KEY = "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"
+MONGODB_URI = os.getenv("MONGODB_URI")
+SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_change_in_production")
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-CLOUDINARY_CLOUD_NAME = "dfzdjuuwc"
-CLOUDINARY_API_KEY = "372996284738846"
-CLOUDINARY_API_SECRET = "JBwzNN6hetOx1NKGfOlXgfta4gQ"
-CLOUDINARY_UPLOAD_PRESET = "parsewave" 
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+
+ZEROBOUNCE_API_KEY = os.getenv("ZEROBOUNCE_API_KEY")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
+CLOUDINARY_UPLOAD_PRESET = os.getenv("CLOUDINARY_UPLOAD_PRESET")
